9d9998b28d26bb8c9f3ce94783028e5b4b1e09f2
Gaurav Parmar <parmargaurav532@gmail.com>
2022-07-07 15:54:36 +0530

    Router setup files

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,21 @@ var path = require("path");
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
-// console.log("source Dir, :>> ", SRC_DIR);
-
 var config = {
-    devtool : 'source-map',
-    entry : SRC_DIR + "/app/index.js",
-    output : {
-        path : DIST_DIR + "/app",
-        filename : "bundle.js",
-        publicPath : "/app/"
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
     },
-    module : {
-        loaders  : [
+    module: {
+        loaders: [
             {
-                test : /\.js?/,
-                include : SRC_DIR,
-                loader : "babel-loader",
-                query : {
-                    presets : ["react", "es2015", "stage-2"]
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
                 }
             }
         ]
